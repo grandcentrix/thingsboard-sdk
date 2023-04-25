75268acad9898b07fb3904a4a264d5090bb6a8cc
René Beckmann <rene.beckmann@grandcentrix.net>
2023-04-25 15:58:14 +0200

    Add black to Python CI
    While Ruff is a linter, black is a formatting checker - why not both?
    Also disables Ruff's line length checking because apparently black
    and ruff can't agree.
    
    ...and fix formatting

--- scripts/gen_json_parser.py
+++ scripts/gen_json_parser.py
@@ -9,7 +9,7 @@
 # others will be specific to the application. Users can provide a JSON schema
 # for their application specific data and this script creates one big structure
 # and json_obj_descr array to parse them.
-# 
+#
 # Some notes:
 # - The script currently expects all JSON schemas to be "type": "object" on the top
 #   level. That's okay for this use case, Thingsboard doesn't supply anything else.
@@ -28,150 +28,160 @@ import os
 
 
 class Property:
-	def __init__(self, name, schema):
-		self.parent = None
-		self._name = name
-		self.schema = schema
+    def __init__(self, name, schema):
+        self.parent = None
+        self._name = name
+        self.schema = schema
 
-	@property
-	def name(self):
-		return self._name
+    @property
+    def name(self):
+        return self._name
 
-	def make_descriptor(self):
-		raise NotImplementedError()
+    def make_descriptor(self):
+        raise NotImplementedError()
 
-	def make_member(self):
-		raise NotImplementedError()
+    def make_member(self):
+        raise NotImplementedError()
 
-	def root_property(self):
-		if not self.parent:
-			return self
-		return self.parent
+    def root_property(self):
+        if not self.parent:
+            return self
+        return self.parent
 
-	def module_name(self):
-		return f"{self.root_property().name}_parser"
+    def module_name(self):
+        return f"{self.root_property().name}_parser"
 
-	def index_define_name(self):
-		return f"{self.module_name().upper()}_{self._name.upper()}_DESCR_INDEX"
+    def index_define_name(self):
+        return f"{self.module_name().upper()}_{self._name.upper()}_DESCR_INDEX"
 
-	@classmethod
-	def create(cls, name, schema):
-		klass = None
-		type = schema["type"]
-		if type == "object":
-			klass = ObjectProperty
-		elif type == "string":
-			klass = StringProperty
-		elif type == "number":
-			klass = NumberProperty
-		else:
-			raise TypeError(f"Type {type} not supported")
-		return klass(name, schema)
+    @classmethod
+    def create(cls, name, schema):
+        klass = None
+        type = schema["type"]
+        if type == "object":
+            klass = ObjectProperty
+        elif type == "string":
+            klass = StringProperty
+        elif type == "number":
+            klass = NumberProperty
+        else:
+            raise TypeError(f"Type {type} not supported")
+        return klass(name, schema)
 
 
 class ObjectProperty(Property):
-	def __init__(self, name, schema):
-		super().__init__(name, schema)
-		self.properties = list()
-		for prop, s in schema["properties"].items():
-			self.add_property(Property.create(prop, s))
+    def __init__(self, name, schema):
+        super().__init__(name, schema)
+        self.properties = list()
+        for prop, s in schema["properties"].items():
+            self.add_property(Property.create(prop, s))
 
-	@property
-	def name(self):
-		if self.parent and self.parent.name:
-			return f"{self.parent.name}_{self._name}"
-		return self._name
+    @property
+    def name(self):
+        if self.parent and self.parent.name:
+            return f"{self.parent.name}_{self._name}"
+        return self._name
 
-	def make_descriptor(self):
-		return f"JSON_OBJ_DESCR_OBJECT(struct {self.parent.name}, " \
-		f"{self._name}, {self.name}_desc)"
+    def make_descriptor(self):
+        return (
+            f"JSON_OBJ_DESCR_OBJECT(struct {self.parent.name}, "
+            f"{self._name}, {self.name}_desc)"
+        )
 
-	def make_member(self):
-		return f"struct {self.name} {self._name};"
+    def make_member(self):
+        return f"struct {self.name} {self._name};"
 
-	def add_property(self, prop):
-		self.properties.append(prop)
-		prop.parent = self
+    def add_property(self, prop):
+        self.properties.append(prop)
+        prop.parent = self
 
-	def merge_with(self, prop):
-		if not isinstance(prop, ObjectProperty):
-			raise TypeError(f"{prop} is not an {self.__class__.__name__}")
-		for p in prop.properties:
-			self.add_property(p)
+    def merge_with(self, prop):
+        if not isinstance(prop, ObjectProperty):
+            raise TypeError(f"{prop} is not an {self.__class__.__name__}")
+        for p in prop.properties:
+            self.add_property(p)
 
-	def make_struct(self):
-		delim = "\n\t"
+    def make_struct(self):
+        delim = "\n\t"
 
-		child_structs = ""
+        child_structs = ""
 
-		for p in self.properties:
-			if isinstance(p, ObjectProperty):
-				child_structs += p.make_struct() + "\n\n"
+        for p in self.properties:
+            if isinstance(p, ObjectProperty):
+                child_structs += p.make_struct() + "\n\n"
 
-		if not self.parent:
-			flags = f"""\
+        if not self.parent:
+            flags = f"""\
 {delim.join(map(lambda p: f"bool {p._name}_parsed;", self.properties))}
 \t"""
-		else:
-			flags = ""
+        else:
+            flags = ""
 
-		return child_structs + f"""\
+        return (
+            child_structs
+            + f"""\
 struct {self.name} {{
 \t{flags}{delim.join([p.make_member() for p in self.properties])}
 }};\
 """
+        )
 
-	def make_descriptors(self):
-		delim = ",\n\t"
+    def make_descriptors(self):
+        delim = ",\n\t"
 
-		child_descr = ""
+        child_descr = ""
 
-		for p in self.properties:
-			if isinstance(p, ObjectProperty):
-				child_descr += p.make_descriptors() + "\n\n"
+        for p in self.properties:
+            if isinstance(p, ObjectProperty):
+                child_descr += p.make_descriptors() + "\n\n"
 
-		return child_descr + f"""\
+        return (
+            child_descr
+            + f"""\
 static const struct json_obj_descr {self.name}_desc[] = {{
 	{delim.join([d.make_descriptor() for d in self.properties])}
 }};\
 """
+        )
 
-	def make_indices(self):
-		def index(i, prop):
-			return f"#define {prop.index_define_name()} {i}"
-		return "\n".join(index(*t) for t in enumerate(self.properties))
+    def make_indices(self):
+        def index(i, prop):
+            return f"#define {prop.index_define_name()} {i}"
+
+        return "\n".join(index(*t) for t in enumerate(self.properties))
 
 
 class StringProperty(Property):
-	def make_descriptor(self):
-		return f"JSON_OBJ_DESCR_PRIM(struct {self.parent.name}, {self.name}, JSON_TOK_STRING)"
+    def make_descriptor(self):
+        return f"JSON_OBJ_DESCR_PRIM(struct {self.parent.name}, {self.name}, JSON_TOK_STRING)"
 
-	def make_member(self):
-		return f"char *{self.name};"
+    def make_member(self):
+        return f"char *{self.name};"
 
 
 class NumberProperty(Property):
-	def make_descriptor(self):
-		return f"JSON_OBJ_DESCR_PRIM(struct {self.parent.name}, {self.name}, JSON_TOK_NUMBER)"
+    def make_descriptor(self):
+        return f"JSON_OBJ_DESCR_PRIM(struct {self.parent.name}, {self.name}, JSON_TOK_NUMBER)"
 
-	def make_member(self):
-		return f"int32_t {self.name};"
+    def make_member(self):
+        return f"int32_t {self.name};"
 
 
 def declare_parser(prop):
-	return f"int {prop.name}_from_json(void *json, size_t len, struct {prop.name} *v)"
+    return f"int {prop.name}_from_json(void *json, size_t len, struct {prop.name} *v)"
 
 
 def define_parser(prop):
-	delim = '\n'
+    delim = "\n"
 
-	def parsed_check(prop):
-		return f"""\
+    def parsed_check(prop):
+        return f"""\
 	if (ret & (1 << {prop.index_define_name()})) {{
 		v->{prop._name}_parsed = true;
 	}}
 """
-	return f"""{declare_parser(prop)} {{
+
+    return f"""{declare_parser(prop)} {{
 	int ret;
 
 	ret = json_obj_parse(json, len, {prop.name}_desc, ARRAY_SIZE({prop.name}_desc), v);
@@ -186,12 +196,13 @@ def define_parser(prop):
 
 
 def write_header(path, prop):
-	filename = prop.module_name() + ".h"
-	guard = prop.module_name().upper() + "_H"
-	outpath = os.path.abspath(os.path.join(path, filename))
-	print(f"Generating {outpath}")
-	with open(outpath, 'w') as h:
-		h.write(f"""\
+    filename = prop.module_name() + ".h"
+    guard = prop.module_name().upper() + "_H"
+    outpath = os.path.abspath(os.path.join(path, filename))
+    print(f"Generating {outpath}")
+    with open(outpath, "w") as h:
+        h.write(
+            f"""\
 #ifndef {guard}
 #define {guard}
 
@@ -205,15 +216,17 @@ def write_header(path, prop):
 
 {declare_parser(prop)};
 
-#endif /* {guard} */""")
+#endif /* {guard} */"""
+        )
 
 
 def write_source(path, prop):
-	filename = prop.module_name() + ".c"
-	outpath = os.path.abspath(os.path.join(path, filename))
-	print(f"Generating {outpath}")
-	with open(outpath, 'w') as src:
-		src.write(f"""\
+    filename = prop.module_name() + ".c"
+    outpath = os.path.abspath(os.path.join(path, filename))
+    print(f"Generating {outpath}")
+    with open(outpath, "w") as src:
+        src.write(
+            f"""\
 #include "{prop.module_name() + ".h"}"
 
 #include <data/json.h>
@@ -221,32 +234,32 @@ def write_source(path, prop):
 {prop.make_descriptors()}
 
 {define_parser(prop)}
-""")
+"""
+        )
 
 
 def load_schema(path):
-	filename = os.path.basename(path)
-	schema_name, ext = os.path.splitext(filename)
-	print(f"Loading JSON schema from {path}")
+    filename = os.path.basename(path)
+    schema_name, ext = os.path.splitext(filename)
+    print(f"Loading JSON schema from {path}")
 
-	# normalize references
-	schema = jrd.RefDict(path)
-	schema = jrd.materialize(schema)
+    # normalize references
+    schema = jrd.RefDict(path)
+    schema = jrd.materialize(schema)
 
-	prop = Property.create(schema_name, schema)
-	if not isinstance(prop, ObjectProperty):
-		raise TypeError("prop is of wrong type")
-	return prop
+    prop = Property.create(schema_name, schema)
+    if not isinstance(prop, ObjectProperty):
+        raise TypeError("prop is of wrong type")
+    return prop
 
 
-if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		raise ValueError("Usage: <outpath> JSON schema file(s)...")
-	outpath = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise ValueError("Usage: <outpath> JSON schema file(s)...")
+    outpath = sys.argv[1]
 
-	prop = load_schema(sys.argv[2])
-	for path in sys.argv[3:]:
-		prop.merge_with(load_schema(path))
-	write_header(outpath, prop)
-	write_source(outpath, prop)
-
+    prop = load_schema(sys.argv[2])
+    for path in sys.argv[3:]:
+        prop.merge_with(load_schema(path))
+    write_header(outpath, prop)
+    write_source(outpath, prop)
