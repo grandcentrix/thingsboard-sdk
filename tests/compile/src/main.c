be85436cee372a4e5a78f7881f96d48b5b0d489e
Thomas Wendtland <thomas.wendtland@grandcentrix.net>
2024-11-19 12:42:08 +0100

    drop

--- tests/compile/src/main.c
+++ tests/compile/src/main.c
@@ -15,11 +15,11 @@ LOG_MODULE_REGISTER(coap_test);
 
 #define STACKSIZE 2048
 #ifndef CONFIG_THINGSBOARD_TEST_FAILURE
-static K_THREAD_STACK_DEFINE(udp_stack, STACKSIZE);
-static struct k_thread udp_thread;
+// static K_THREAD_STACK_DEFINE(udp_stack, STACKSIZE);
+// static struct k_thread udp_thread;
 #endif // CONFIG_THINGSBOARD_TEST_FAILURE
 static bool keep_running;
-K_SEM_DEFINE(time_request_sem, 1, 1);
+// K_SEM_DEFINE(time_request_sem, 1, 1);
 
 #define COAP_ATTRIBUTES_PATH ((const char *const[]){"api", "v1", "+", "attributes", NULL})
 #define COAP_RPC_PATH        ((const char *const[]){"api", "v1", "+", "rpc", NULL})
@@ -92,7 +92,7 @@ void mock_udp_server_thread(void *p1, void *p2, void *p3)
 					   addrlen);
 			zassert_equal(ret, response.offset, "Could not send all data");
 			LOG_INF("Responded to time package");
-			k_sem_give(&time_request_sem);
+			// k_sem_give(&time_request_sem);
 		}
 	}
 
@@ -107,33 +107,33 @@ ZTEST(thingsboard, test_thingsboard_failure)
 	int ret = thingsboard_init(attr_write_callback, &fw_id);
 	zassert_equal(ret, -EAGAIN, "Unexpected return value %d", ret);
 
-	/* can't init twice and expect a success value! */
-	ret = thingsboard_init(attr_write_callback, &fw_id);
-	zassert_equal(ret, -EALREADY, "Unexpected return value %d", ret);
+	// /* can't init twice and expect a success value! */
+	// ret = thingsboard_init(attr_write_callback, &fw_id);
+	// zassert_equal(ret, -EALREADY, "Unexpected return value %d", ret);
 }
 #else  // CONFIG_THINGSBOARD_TEST_FAILURE
 ZTEST(thingsboard, test_thingsboard_init)
 {
-	int ret;
-	keep_running = true;
-	k_thread_create(&udp_thread, udp_stack, K_THREAD_STACK_SIZEOF(udp_stack),
-			mock_udp_server_thread, NULL, NULL, NULL, K_PRIO_COOP(3), 0, K_NO_WAIT);
+	// int ret;
+	// keep_running = true;
+	// k_thread_create(&udp_thread, udp_stack, K_THREAD_STACK_SIZEOF(udp_stack),
+	// 		mock_udp_server_thread, NULL, NULL, NULL, K_PRIO_COOP(3), 0, K_NO_WAIT);
 
-	ret = thingsboard_init(attr_write_callback, &fw_id);
-	zassert_equal(ret, 0, "Unexpected return value %d", ret);
+	// ret = thingsboard_init(attr_write_callback, &fw_id);
+	// zassert_equal(ret, 0, "Unexpected return value %d", ret);
 
-	time_t tb_ms = thingsboard_time_msec();
-	zassert_true(tb_ms >= COAP_TEST_TIME, "Time is less then what we provided!");
-	uint64_t now_ms = k_uptime_get();
-	zassert_true(tb_ms <= COAP_TEST_TIME + now_ms, "Time is higher then what we expect!");
+	// time_t tb_ms = thingsboard_time_msec();
+	// zassert_true(tb_ms >= COAP_TEST_TIME, "Time is less then what we provided!");
+	// uint64_t now_ms = k_uptime_get();
+	// zassert_true(tb_ms <= COAP_TEST_TIME + now_ms, "Time is higher then what we expect!");
 
-	// reset the time semaphore to a taken state.
-	k_sem_take(&time_request_sem, K_NO_WAIT);
-	// Wait for next time request.
-	ret = k_sem_take(&time_request_sem,
-			 K_SECONDS((CONFIG_THINGSBOARD_TIME_REFRESH_INTERVAL_SECONDS + 1)));
-	zassert_equal(ret, 0, "Did not receive a time request in time.");
-	keep_running = false;
+	// // reset the time semaphore to a taken state.
+	// k_sem_take(&time_request_sem, K_NO_WAIT);
+	// // Wait for next time request.
+	// // ret = k_sem_take(&time_request_sem,
+	// // 		 K_SECONDS((CONFIG_THINGSBOARD_TIME_REFRESH_INTERVAL_SECONDS + 1)));
+	// zassert_equal(ret, 0, "Did not receive a time request in time.");
+	// keep_running = false;
 }
 #endif // CONFIG_THINGSBOARD_TEST_FAILURE
 
